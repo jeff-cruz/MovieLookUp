768986d92f50023a55023273c0791f599ef8107d
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-10-12 23:34:55 +0000

    Able to obtain movie data from API

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import Home from './pages/home';
+import React, { useEffect } from 'react';
+import '../server/public/styles.css';
 
-export default class App extends React.Component {
-  render() {
-    return <Home />;
-  }
-}
+const API_URL = 'http://www.omdbapi.com?apikey=a0584c07';
+
+const App = () => {
+
+  const searchMovies = async title => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
+  return (
+    <h1> hello there how is it going</h1>
+  );
+};
+
+export default App;
