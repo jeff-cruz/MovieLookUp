90534ba485d670eefae280fdd4b43709ca4b5182
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-10-13 00:47:41 +0000

    Created movie cards and mapped over movie list

--- client/app.jsx
+++ client/app.jsx
@@ -1,15 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../server/public/styles.css';
+import Card from './card';
 
 const API_URL = 'http://www.omdbapi.com?apikey=a0584c07';
 
+const movie1 = {
+  Title: 'Spiderman',
+  Year: '2010',
+  imdbID: 'tt1785572',
+  Type: 'movie',
+  Poster: 'N/A'
+};
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async title => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -17,7 +27,31 @@ const App = () => {
   }, []);
 
   return (
-    <h1> hello there how is it going</h1>
+    <div className='app'>
+      <h1>Movies</h1>
+      <div className='search'>
+        <input
+          placeholder="Search here..."
+          value="Superman"
+          onChange={() => {}}
+        />
+        <button onClick={() => {}}> Search </button>
+      </div>
+
+      {movies?.length > 0
+        ? (
+          <div className='container'>
+            {movies.map(movie => (
+              <Card movie={movie} key={movie}/>
+            ))}
+          </div>
+          )
+        : (
+          <div className='empty'>
+            <h2>No movies found</h2>
+          </div>
+          )}
+    </div>
   );
 };
 
